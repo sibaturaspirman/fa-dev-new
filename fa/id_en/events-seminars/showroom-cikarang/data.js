f9350c60c6ecfbbd2af4781385740ddbf4913cea
Cecep Aprilianto <cecepaprilianto@gmail.com>
2025-07-02 10:13:10 +0700

    fix: No.23 - Content Exist but Not Related

--- fa/id_en/events-seminars/showroom-cikarang/data.js
+++ fa/id_en/events-seminars/showroom-cikarang/data.js
@@ -2,37 +2,44 @@ let datas = [
     {
       "URL": "/fa/id_en/events-seminars/showroom-cikarang/B-1.html",
       "Image": "/fa/id_en/events-seminars/assets/img/showroom/B-1.jpg",
-      "Title": "Digitalization of Smart Manufacturing for Production Line"
+      "Title": "Digitalization of Smart Manufacturing for Production Line",
+        "Virtuals": [1],
     },
     {
       "URL": "/fa/id_en/events-seminars/showroom-cikarang/B-2.html",
       "Image": "/fa/id_en/events-seminars/assets/img/showroom/B-2.jpg",
-      "Title": "Collaborative Robot Barista"
+      "Title": "Collaborative Robot Barista",
+        "Virtuals": [1],
     },
     {
       "URL": "/fa/id_en/events-seminars/showroom-cikarang/B-3.html",
       "Image": "/fa/id_en/events-seminars/assets/img/showroom/B-3.jpg",
-      "Title": "HVAC Redundancy Control System"
+      "Title": "HVAC Redundancy Control System",
+        "Virtuals": [1],
     },
     {
       "URL": "/fa/id_en/events-seminars/showroom-cikarang/B-5.html",
       "Image": "/fa/id_en/events-seminars/assets/img/showroom/B-5.jpg",
-      "Title": "Engraving CNC Demo Machine"
+      "Title": "Engraving CNC Demo Machine",
+        "Virtuals": [1],
     },
     {
       "URL": "/fa/id_en/events-seminars/showroom-cikarang/B-6.html",
       "Image": "/fa/id_en/events-seminars/assets/img/showroom/B-6.jpg",
-      "Title": "High-performance Wire-cut EDM MV-R Series"
+      "Title": "High-performance Wire-cut EDM MV-R Series",
+        "Virtuals": [],
     },
     {
       "URL": "/fa/id_en/events-seminars/showroom-cikarang/B-7.html",
       "Image": "/fa/id_en/events-seminars/assets/img/showroom/B-7.jpg",
-      "Title": "High-performance Die-Sinker EDM SG Series"
+      "Title": "High-performance Die-Sinker EDM SG Series",
+        "Virtuals": [],
     },
     {
       "URL": "/fa/id_en/events-seminars/showroom-cikarang/B-8.html",
       "Image": "/fa/id_en/events-seminars/assets/img/showroom/B-8.jpg",
-      "Title": "Pick and Place Robot Candy"
+      "Title": "Pick and Place Robot Candy",
+        "Virtuals": [],
     },
     // {
     //   "URL": "/fa/id_en/events-seminars/showroom-cikarang/B-9.html",
@@ -42,7 +49,8 @@ let datas = [
     {
       "URL": "/fa/id_en/events-seminars/showroom-cikarang/B-10.html",
       "Image": "/fa/id_en/events-seminars/assets/img/showroom/B-10.jpg",
-      "Title": "Oil and Gas Solution"
+      "Title": "Oil and Gas Solution",
+        "Virtuals": [],
     },
     // {
     //   "URL": "/fa/id_en/events-seminars/showroom-cikarang/B-11.html",
@@ -52,17 +60,20 @@ let datas = [
     {
       "URL": "/fa/id_en/events-seminars/showroom-cikarang/B-12.html",
       "Image": "/fa/id_en/events-seminars/assets/img/showroom/B-12.jpg",
-      "Title": "Carbon Neutral Solution (Energy Monitoring System)"
+      "Title": "Carbon Neutral Solution (Energy Monitoring System)",
+        "Virtuals": [],
     },
     {
       "URL": "/fa/id_en/events-seminars/showroom-cikarang/B-13.html",
       "Image": "/fa/id_en/events-seminars/assets/img/showroom/B-13.jpg",
-      "Title": "CNC In-house Repair"
+      "Title": "CNC In-house Repair",
+        "Virtuals": [1],
     },
     {
       "URL": "/fa/id_en/events-seminars/showroom-cikarang/B-14.html",
       "Image": "/fa/id_en/events-seminars/assets/img/showroom/B-14.jpg",
-      "Title": "Pick and Place Robot with Rotary and Linear Synchronize Application"
+      "Title": "Pick and Place Robot with Rotary and Linear Synchronize Application",
+        "Virtuals": [],
     }
 ];
 
@@ -71,7 +82,15 @@ datas.sort(function (a, b) {
 });
 
 $(document).ready(function () {
-    $.each(datas, function (i, item) {
+    const virtualAttr = $('#data-showroom').data('virtual');
+    const filteredDatas = datas.filter(item => {
+        if (typeof virtualAttr !== 'undefined') {
+            return item.Virtuals.includes(virtualAttr);
+        }
+        return true; // if not exists data-virtual, show all
+    });
+
+    $.each(filteredDatas, function (i, item) {
         const html = `
         <div class="l-grid__item l-grid__item-3 l-grid__item-6-md l-grid__item-6-sm">
             <a class="c-linkWithImage" href="${item.URL}">
--- fa/id_id/events-seminars/showroom-cikarang/data.js
+++ fa/id_id/events-seminars/showroom-cikarang/data.js
@@ -2,37 +2,44 @@ let datas = [
     {
       "URL": "/fa/id_id/events-seminars/showroom-cikarang/B-1.html",
       "Image": "/fa/id_id/events-seminars/assets/img/showroom/B-1.jpg",
-      "Title": "Digitalisasi Manufaktur Cerdas untuk Lini Produksi"
+      "Title": "Digitalisasi Manufaktur Cerdas untuk Lini Produksi",
+        "Virtuals": [1],
     },
     {
       "URL": "/fa/id_id/events-seminars/showroom-cikarang/B-2.html",
       "Image": "/fa/id_id/events-seminars/assets/img/showroom/B-2.jpg",
-      "Title": "Robot Barista Kolaboratif"
+      "Title": "Robot Barista Kolaboratif",
+        "Virtuals": [1],
     },
     {
       "URL": "/fa/id_id/events-seminars/showroom-cikarang/B-3.html",
       "Image": "/fa/id_id/events-seminars/assets/img/showroom/B-3.jpg",
-      "Title": "Sistem Kontrol Redundansi HVAC"
+      "Title": "Sistem Kontrol Redundansi HVAC",
+        "Virtuals": [1],
     },
     {
       "URL": "/fa/id_id/events-seminars/showroom-cikarang/B-5.html",
       "Image": "/fa/id_id/events-seminars/assets/img/showroom/B-5.jpg",
-      "Title": "Mesin Demo Ukiran CNC"
+      "Title": "Mesin Demo Ukiran CNC",
+        "Virtuals": [1],
     },
     {
       "URL": "/fa/id_id/events-seminars/showroom-cikarang/B-6.html",
       "Image": "/fa/id_id/events-seminars/assets/img/showroom/B-6.jpg",
-      "Title": "Kawat potong berkinerja tinggi EDM MV-R Series"
+      "Title": "Kawat potong berkinerja tinggi EDM MV-R Series",
+        "Virtuals": [],
     },
     {
       "URL": "/fa/id_id/events-seminars/showroom-cikarang/B-7.html",
       "Image": "/fa/id_id/events-seminars/assets/img/showroom/B-7.jpg",
-      "Title": "Die-Sinker Berkinerja Tinggi EDM SG Series"
+      "Title": "Die-Sinker Berkinerja Tinggi EDM SG Series",
+        "Virtuals": [],
     },
     {
       "URL": "/fa/id_id/events-seminars/showroom-cikarang/B-8.html",
       "Image": "/fa/id_id/events-seminars/assets/img/showroom/B-8.jpg",
-      "Title": "Pilih dan Tempatkan Permen Robot"
+      "Title": "Pilih dan Tempatkan Permen Robot",
+        "Virtuals": [],
     },
     // {
     //   "URL": "/fa/id_id/events-seminars/showroom-cikarang/B-9.html",
@@ -42,7 +49,8 @@ let datas = [
     {
       "URL": "/fa/id_id/events-seminars/showroom-cikarang/B-10.html",
       "Image": "/fa/id_id/events-seminars/assets/img/showroom/B-10.jpg",
-      "Title": "Solusi Minyak dan Gas"
+      "Title": "Solusi Minyak dan Gas",
+        "Virtuals": [],
     },
     // {
     //   "URL": "/fa/id_id/events-seminars/showroom-cikarang/B-11.html",
@@ -52,17 +60,20 @@ let datas = [
     {
       "URL": "/fa/id_id/events-seminars/showroom-cikarang/B-12.html",
       "Image": "/fa/id_id/events-seminars/assets/img/showroom/B-12.jpg",
-      "Title": "Solusi Netral Karbon (Sistem Pemantauan Energi)"
+      "Title": "Solusi Netral Karbon (Sistem Pemantauan Energi)",
+        "Virtuals": [],
     },
     {
       "URL": "/fa/id_id/events-seminars/showroom-cikarang/B-13.html",
       "Image": "/fa/id_id/events-seminars/assets/img/showroom/B-13.jpg",
-      "Title": "Perbaikan CNC Internal"
+      "Title": "Perbaikan CNC Internal",
+        "Virtuals": [1],
     },
     {
       "URL": "/fa/id_id/events-seminars/showroom-cikarang/B-14.html",
       "Image": "/fa/id_id/events-seminars/assets/img/showroom/B-14.jpg",
-      "Title": "Robot Pick and Place dengan Aplikasi Sinkronisasi Rotary dan Linear"
+      "Title": "Robot Pick and Place dengan Aplikasi Sinkronisasi Rotary dan Linear",
+        "Virtuals": [],
     }
 ];
 
@@ -71,7 +82,15 @@ datas.sort(function (a, b) {
 });
 
 $(document).ready(function () {
-    $.each(datas, function (i, item) {
+    const virtualAttr = $('#data-showroom').data('virtual');
+    const filteredDatas = datas.filter(item => {
+        if (typeof virtualAttr !== 'undefined') {
+            return item.Virtuals.includes(virtualAttr);
+        }
+        return true; // if not exists data-virtual, show all
+    });
+
+    $.each(filteredDatas, function (i, item) {
         const html = `
         <div class="l-grid__item l-grid__item-3 l-grid__item-6-md l-grid__item-6-sm">
             <a class="c-linkWithImage" href="${item.URL}">
