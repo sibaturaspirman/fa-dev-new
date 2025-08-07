57c862158890581f34d9a7eb6c4a33848ba9c95c
Asep Irman <asepirman@antigravity.id>
2025-08-07 16:46:43 +0700

    up

--- fa/id_en/shared/gws0001/js/init.js
+++ fa/id_en/shared/gws0001/js/init.js
@@ -660,15 +660,15 @@
 		document.body.insertBefore(gtmNoscript, document.body.firstChild);
 
 		// SCRIPT WA
-		// const qchatInit = document.createElement('script');
-		// qchatInit.src = "https://webchat.qontak.com/qchatInitialize.js";
-		// const qchatWidget = document.createElement('script');
-		// qchatWidget.src = "https://webchat.qontak.com/js/app.js";
-		// document.head.prepend(qchatInit);
-		// document.head.prepend(qchatWidget);
-		// qchatInit.onload = function() { qchatInitialize({
-		// 	id: "d7adcebe-8ffa-45b2-b10e-b3de46374d74", 
-		// 	code: "9850_eFpx20FNrXv3oYysg"  
-		// })};
+		const qchatInit = document.createElement('script');
+		qchatInit.src = "https://webchat.qontak.com/qchatInitialize.js";
+		const qchatWidget = document.createElement('script');
+		qchatWidget.src = "https://webchat.qontak.com/js/app.js";
+		document.head.prepend(qchatInit);
+		document.head.prepend(qchatWidget);
+		qchatInit.onload = function() { qchatInitialize({
+			id: "d7adcebe-8ffa-45b2-b10e-b3de46374d74", 
+			code: "9850_eFpx20FNrXv3oYysg"  
+		})};
 	});
 })(window.jQuery3_6 || jQuery);
